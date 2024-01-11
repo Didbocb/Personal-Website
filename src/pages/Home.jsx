73d3f1757b1386cf8ef03fc28056c345004d4e7d
Lado Natsvlishvili <ladonatsvlishvili@gmail.com>
2024-01-11 14:01:51 -0500

    added some text based changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,7 +37,7 @@ export default function Home({ data }) {
                 <h2 className="section-header">Introduction</h2>
 
                 <p>
-                    Greetings! I'm Lado, a passionate second-year explorer navigating the realm of Computer Science at the University of Guelph. Balancing my academic pursuits with real-world experience, I'm on a Co-op journey, delving into the intricacies of Computer Science. Outside the coding sphere, I find solace in the harmonies of music and the thrill of racing games. In the pockets of free time, I embark on ventures of creativity, crafting personal projects to elevate my prowess in programming and design. <br /><br />
+                    Greetings! My name is Lado and I'm a passionate second-year student navigating the realm of Computer Science at the University of Guelph. Balancing my academic pursuits with real-world experience, I'm on a Co-op journey, delving into the intricacies of Computer Science. Outside the coding sphere, I find solace in the harmonies of music and the thrill of racing games. In the pockets of free time, I embark on ventures of creativity, crafting personal projects to elevate my prowess in programming and design. <br /><br />
                     If you resonate with my journey or simply want to connect, feel free to reach out through any of the platforms below.
                 </p>
 
